4742425d5120d61ea5f29489505bc1a5def90c11
Takapa <maiga.maxime@hotmail.fr>
2015-12-21 18:39:42 +0100

    v1.1

--- MusicDuplicate/src/duplicate/FindTheFuckers.java
+++ MusicDuplicate/src/duplicate/FindTheFuckers.java
@@ -2,6 +2,10 @@ package duplicate;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 public class FindTheFuckers {
 	File folder = new File("C:\\Program Files (x86)\\UltraStar Deluxe\\test");
@@ -12,11 +16,13 @@ public class FindTheFuckers {
 	String sSong;
 	int iIndex;
 	int iDash;
+	String[] sSongWords;
+	List<String> lstSongWords = new ArrayList<String>();
 	ArrayList<String> alArtistFound = new ArrayList<String>();
 
 	public void FindThemAll(){
-		for (int i = 0; i < 1; i++) {
-			System.out.println(listOfFiles[i].toString());
+		for (int i = 0; i < 4; i++) {
+			System.out.println("New Artist "+ listOfFiles[i].toString());
 			sCurrentFolder = listOfFiles[i].toString();
 			iIndex = sCurrentFolder.indexOf(" -");
 			iDash = sCurrentFolder.lastIndexOf("\\");
@@ -27,7 +33,7 @@ public class FindTheFuckers {
 				System.out.println(sSplittedArtist);
 			}
 			for (int j = 0; j < listOfFiles.length; j++){
-				if(listOfFiles[j].toString().matches("(.*)"+sSplittedArtist+"(.*)|(.*)"+sSplittedArtist.toLowerCase()+"(.*)|(.*)"+sSplittedArtist.substring(0, 1).toUpperCase() + sSplittedArtist.substring(1).toLowerCase()+"(.*)")){
+				if(listOfFiles[j].toString().matches("(.*)"+sSplittedArtist+"(.*)|(.*)"+sSplittedArtist.toLowerCase()+"(.*)|(.*)"+sSplittedArtist.substring(0, 1).toUpperCase() + sSplittedArtist.substring(1).toLowerCase()+"(.*)|(.*)"+sSplittedArtist.toUpperCase()+"(.*)")){
 					//System.out.println(sSplittedArtist);
 					alArtistFound.add(sSplittedArtist);
 					sLookingFolder = listOfFiles[j].toString();
@@ -35,6 +41,16 @@ public class FindTheFuckers {
 					System.out.println(listOfFiles[j].toString());
 					sSong = sLookingFolder.substring(iIndex+3);
 					System.out.println(sSong);
+					sSongWords = sSong.split("[[ ]*|[,]]+");
+					for (String sWord : sSongWords){
+						System.out.println(sWord);
+						lstSongWords.add(sWord);
+					}
+					Set<String> unique = new HashSet<String>(lstSongWords);
+					for (String key : unique) {
+					    System.out.println(key + ": " + Collections.frequency(lstSongWords, key));
+					}
+					lstSongWords.clear();
 				}
 			}
 		}
